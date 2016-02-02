ae55a0f6033848cd70b0c507023c5ab525e60b60
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-02-02 15:12:08 -0800

    added _byteIndex wich calculates the byte number of this.buffer for a given bit index. This is different than just using index>>>3 because it takes endianness into account and keeps the multi-byte bit stream in the correct order.

--- bitbuffer.js
+++ bitbuffer.js
@@ -1,34 +1,54 @@
 "use strict"
+var os = require('os');
 
 function BitBuffer(number, buffer) {
 	var size = Math.ceil(number / 8)
+  
 	if (buffer != undefined && buffer.length == size) {
 		this.buffer = buffer
 	} else {
 		this.buffer = new Buffer(size)
 		this.buffer.fill(0)
 	}
+  this.maxByteIndex = this.buffer.length - 1;
+  this.size = this.buffer.length * 8;
+  
+  /*
+    If this is a little endian system, byte 0 would be on the left hand side
+    of the buffer. However, since this is supposed to represend a bit array,
+    we should foce a big endian layout by reversing the byte order so byte 0
+    is on the right. 
+  */
+  this._byteIndex = os.endianness() == "LE" ?
+    this._byteIndexLE : this._byteIndexBE; 
 }
 
 
 BitBuffer.prototype = {
 	set: function(index, bool) {
-		var pos = index >>> 3
 		if(bool) {
-			this.buffer[pos] |= 1 << (index % 8)
+			this.buffer[this._byteIndex(index)] |= 1 << (index % 8);
 		} else {
-			this.buffer[pos] &= ~(1 << (index % 8))
+			this.buffer[this._byteIndex(index)] &= ~(1 << (index % 8));
 		}
 	},
 	get: function(index) {
-		return (this.buffer[index >>> 3] & (1 << (index % 8))) != 0
+    return (this.buffer[this._byteIndex(index)] & (1 << (index % 8))) != 0;
 	},
 	toggle: function(index) {
-		this.buffer[index >>> 3] ^= 1 << (index % 8)
+		this.buffer[this._byteIndex(index)] ^= 1 << (index % 8);
 	},
 	toBuffer: function() {
 		return this.buffer
-	}
+	},
+  
+  _byteIndex: null,
+  _byteIndexLE: function(index) {
+    return this.maxByteIndex - (index >>> 3);
+  },
+  _byteIndexBE: function(index) {
+    return index >>> 3;
+  }
 }
 
 exports.BitBuffer = BitBuffer
