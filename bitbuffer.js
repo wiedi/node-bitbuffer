ec1a3fc346475c8a740489d4af9cec4e91958ca5
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-02-03 14:50:11 -0800

    subbuffer now uses two buffers and loops over the needed area rather than creating a bit array, slicing, then converting back to a bitbuffer

--- bitbuffer.js
+++ bitbuffer.js
@@ -188,7 +188,23 @@ BitBuffer.prototype = {
     return this;
   },
   subbuffer: function(begin, end) {
-    return (new BitBuffer()).fromBitArray(this.toBitArray().slice(begin, end));
+    var newbuff, size; 
+    
+    //make sure begin and end are valid
+    begin = isFinite(+begin) ? begin : 0,
+    end = isFinite(+end) ? +end : this.size;
+    begin = begin >= 0 ? begin : 0;
+    end = end <= this.size ? end : this.size;
+    size = end - begin;
+    if (size < 1) {
+      return new BitBuffer(0);
+    }
+    
+    newbuff = new BitBuffer(size);
+    for (var bit_i = 0; bit_i < size; bit_i++) {
+      newbuff.set(bit_i, this.get(bit_i + begin));
+    }
+    return newbuff;
   },
   shiftRight: function(shiftBits) {
     if (shiftBits < 0) {
