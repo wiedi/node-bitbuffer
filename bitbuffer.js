2cee64d6d206e1d00ffd0668e374203b9e2b7ef9
Warren Rexroad <wrexroad@gmail.com>
2016-02-08 13:33:30 -0800

    added subbuffer

--- bitbuffer.js
+++ bitbuffer.js
@@ -2,14 +2,15 @@
 
 function BitBuffer(number, buffer) {
 	var size = Math.ceil(number / 8)
+	
 	if (buffer != undefined && buffer.length == size) {
 		this.buffer = buffer
 	} else {
 		this.buffer = new Buffer(size)
 		this.buffer.fill(0)
 	}
+	this.size = number
 }
-
 
 BitBuffer.prototype = {
 	set: function(index, bool) {
@@ -28,7 +29,26 @@ BitBuffer.prototype = {
 	},
 	toBuffer: function() {
 		return this.buffer
-	}
+	},
+  subbuffer: function(begin, end) {
+		var newbuff, size 
+		
+		//make sure begin and end are valid
+		begin = isFinite(+begin) ? begin : 0,
+		end = isFinite(+end) ? end : this.size
+		begin = begin >= 0 ? begin : 0
+		end = end <= this.size ? end : this.size
+		size = end - begin
+		if (size < 1) {
+			return new BitBuffer(0)
+		}
+		
+		newbuff = new BitBuffer(size)
+		for (var bit_i = 0; bit_i < size; bit_i++) {
+			newbuff.set(bit_i, this.get(bit_i + begin))
+		}
+		return newbuff
+	},	
 }
 
 exports.BitBuffer = BitBuffer
