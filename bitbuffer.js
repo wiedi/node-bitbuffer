eefbd24d1fac5c6ae960a7fe97bd61a90bdbb24d
Warren Rexroad <wrexroad@gmail.com>
2016-02-08 13:51:22 -0800

    added functions for to and from binary and hex strings

--- bitbuffer.js
+++ bitbuffer.js
@@ -110,6 +110,84 @@ BitBuffer.prototype = {
 		}
 		
 		return bitarr
+	},
+	
+	fromBinaryString: function(bitstr, noresize) {
+		//treat the string as an array of bits that has been indexed backwards
+		var bitSize = bitstr.length, byteSize = Math.ceil(bitSize / 8), bit_i = 0
+		
+		if (bitSize < 1) {
+			return this.resize(0)
+		}
+
+		//clear out the buffer
+		if (noresize || byteSize == this.buffer.length) {
+			this.buffer.fill(0)
+		} else {
+			this.buffer = new Buffer(byteSize)
+			this.buffer.fill(0)
+			this.size = bitSize
+		}
+
+		while (bitSize--) {
+			this.set(bit_i++, !!+bitstr[bitSize])
+		}
+		
+		return this
+	},
+	toBinaryString: function() {
+		return this.toBitArray(-1).join("")
+	},
+ 
+	fromHexString: function(hexstr, noresize) {
+		//treat the string as an array of bits that has been indexed backwards
+		var
+			nybbleSize = hexstr.length,
+			byteSize = Math.ceil(nybbleSize / 2),
+			bitSize = nybbleSize << 2;
+		
+		if (nybbleSize < 1) {
+			return this.resize(0)
+		}
+		
+		//clear out the buffer
+		if (noresize || byteSize == this.buffer.length) {
+			this.buffer.fill(0)
+		} else {
+			this.buffer = new Buffer(byteSize)
+			this.buffer.fill(0)
+			this.size = bitSize
+		}
+		
+		//pad the hex string if it does not contain an integer number of bytes
+		if (nybbleSize % 2 != 0) {
+			hexstr = "0" + hexstr
+			nybbleSize++
+			bitSize += 4
+		}
+		
+		for (var bit_i=bitSize-1, nyb_i=0; nyb_i < nybbleSize; bit_i-=8, nyb_i+=2) {
+			this.buffer[bit_i >>> 3]=+("0x"+hexstr[nyb_i]+hexstr[nyb_i+1])
+		}
+		
+		return this
+	},
+	toHexString: function() {
+		var byte_i = this.buffer.length, hexarr = [], hexstr
+		
+		while (byte_i--) {
+			hexarr.push(
+				(this.buffer[byte_i] < 0x10 ? "0" : "") +
+				this.buffer[byte_i].toString(16)
+			)
+		}
+		
+		hexstr = hexarr.join("")
+	 
+		//the string will be in whole bytes.
+		//However, if our bit buffer size is not in whole bytes,
+		//we should chop off any leading nybbles before returning
+		return hexstr.substring(hexstr.length - (Math.ceil(this.size / 4)))
 	}
 }
 
